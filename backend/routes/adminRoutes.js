bdbbce63a2cfce0f63c43012201601a685bd6ea0
MadisoMelese <madishamadiso00@gmail.com>
2025-05-05 18:06:39 +0300

    gbackend logic for regterar updated

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -19,7 +19,7 @@ router.get('/logout', logoutAdmin);
 router.post("/addStudents", authenticateToken, addStudentCredentials);
 router.post("/upload", authenticateToken, uploadFile); // Ensure this matches your backend route
 
-router.get("/certificates", authenticateToken, getAllCertificates);
+router.get("/certificates", getAllCertificates);
 router.get("/certificates/:certificateID", getSingleCertificate);
 router.delete("/certificates/:certificateID", authenticateToken, deleteSingleCertificate);
 router.put("/certificates/:certificateID/edit", authenticateToken, updateSingleCertificate);
