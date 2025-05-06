a8239c3b4651050f053a61d9bec6ccd40164093e
MadisoMelese <madishamadiso00@gmail.com>
2025-05-06 14:34:13 +0300

    /src/pages/GraduateSearch.jsx modified

--- frontend/src/pages/GraduateSearch.jsx
+++ frontend/src/pages/GraduateSearch.jsx
@@ -198,7 +198,6 @@ const GraduateSearch = () => {
           <option value="2023">2023</option>
           <option value="2024">2024</option>
           <option value="2025">2025</option>
-          <option value="2026">2026</option>
         </select>
       </div>
       <div className="flex justify-center mt-4">
