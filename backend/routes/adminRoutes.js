2d6a07239f16b842ad723f1086f6faf52427882a
Madiso Melese <madishamadiso00@gmail.com>
2025-05-21 04:51:48 +0300

    routes/adminRoutes.js modified

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const {authenticateUser, roleMiddleware, verifyAdminToken} = require("../middleware/authMiddleware");
+const {
+  authenticateUser,
+  roleMiddleware,
+  verifyAdminToken,
+} = require("../middleware/authMiddleware");
 const {
   loginAdmin,
   getAllCertificates,
@@ -18,19 +22,50 @@ const {
 const router = express.Router();
 
 router.post("/login", loginAdmin);
-router.get('/logout', logoutAdmin);
+router.get("/logout", logoutAdmin);
 
-router.post("/createUser", authenticateUser, roleMiddleware(["admin"]), createUserByAdmin);
+router.post(
+  "/createUser",
+  authenticateUser,
+  roleMiddleware(["admin"]),
+  createUserByAdmin
+);
 
-router.post("/addStudents", authenticateUser, addStudentCredentials);
-router.post("/upload", authenticateUser, uploadFile); // Ensure this matches your backend route
+router.post(
+  "/addStudents",
+  authenticateUser,
+  roleMiddleware(["admin", "registrar"]),
+  addStudentCredentials
+);
 
-router.get("/certificates", authenticateUser, getAllCertificates);
+router.post(
+  "/upload",
+  roleMiddleware(["admin", "registrar"]),
+  authenticateUser,
+  uploadFile
+); // Ensure this matches your backend route
+
+router.get(
+  "/certificates",
+  authenticateUser,
+  roleMiddleware(["admin", "registrar"]),
+  getAllCertificates
+);
 router.get("/notifications", getNotification);
 router.delete("/notifications/:id", deleteNotification);
-router.put("/notifications/:id/read",  markNotificationAsRead);
+router.put("/notifications/:id/read", markNotificationAsRead);
 router.get("/certificates/:certificateID", getSingleCertificate);
-router.delete("/certificates/:certificateID", authenticateUser, deleteSingleCertificate);
-router.put("/certificates/:certificateID/edit", authenticateUser, updateSingleCertificate);
+router.delete(
+  "/certificates/:certificateID",
+  authenticateUser,
+  roleMiddleware(["admin", "registrar"]),
+  deleteSingleCertificate
+);
+router.put(
+  "/certificates/:certificateID/edit",
+  authenticateUser,
+  roleMiddleware(["admin", "registrar"]),
+  updateSingleCertificate
+);
 
 module.exports = router;
