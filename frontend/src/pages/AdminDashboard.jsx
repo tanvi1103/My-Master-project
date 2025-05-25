888ce0c15e2b7a22a2f494da3650cd080769d9ac
Madiso Melese <madishamadiso00@gmail.com>
2025-05-25 23:03:24 +0300

    src/pages/AdminDashboard.jsx updated

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -12,6 +12,7 @@ const AdminDashboard = () => {
       </p>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+
         {/* View Graduates */}
         <Link
           to="/admin/view-graduates"
