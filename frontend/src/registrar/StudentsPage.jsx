671d8f76f45a40f445d92f15f3467d5e7a07cb26
Madiso Melese <madishamadiso00@gmail.com>
2025-06-01 04:46:23 +0300

    src/registrar/StudentsPage.jsx updated

--- frontend/src/registrar/StudentsPage.jsx
+++ frontend/src/registrar/StudentsPage.jsx
@@ -484,9 +484,9 @@ const StudentsPage = () => {
                     <td className="px-6 py-4 whitespace-nowrap">
                       <span
                         className={`px-3 py-1 inline-flex text-xs leading-5 font-semibold rounded-full ${
-                          cert.gstatus === "Verified"
+                          cert.gstatus === "verified"
                             ? "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200"
-                            : cert.gstatus === "Pending"
+                            : cert.gstatus === "pending"
                             ? "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200"
                             : "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200"
                         }`}
